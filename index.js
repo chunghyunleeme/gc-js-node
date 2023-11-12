dc6d4c33115e816d5f305624d220e96a5b339b78
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-12 18:31:20 +0900

    feat: write memory-usage file

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function allocateMemory(size) {
   return arr;
 }
 
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+
 const memoryLeakAllocations = [];
 
 const field = "heapUsed";
@@ -26,5 +30,12 @@ setInterval(() => {
   const gbNow = mu[field] / 1024 / 1024 / 1024;
   const gbRounded = Math.round(gbNow * 100) / 100;
 
+  const start = Date.now();
+  const LOG_FILE = path.join(__dirname, "memory-usage.csv");
+
+  const elapsedTimeInSecs = (Date.now() - start) / 1000;
+  const timeRounded = Math.round(elapsedTimeInSecs * 100) / 100;
+
+  fs.appendFile(LOG_FILE, timeRounded + "," + gbRounded + os.EOL, () => {});
   console.log(`Heap allocated ${gbRounded} GB`);
 }, TIME_INTERVAL_IN_MSEC);
