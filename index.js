9637ab0d39494133661cb32f1ccd5c3826e8bc73
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-12 18:19:45 +0900

    feat: allocate heap memory

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function allocateMemory(size) {
   }
   return arr;
 }
+
+const memoryLeakAllocations = [];
+
+const field = "heapUsed";
+const allocationStep = 10000 * 1024; // 10MB
+
+const TIME_INTERVAL_IN_MSEC = 40;
+
+setInterval(() => {
+  const allocation = allocateMemory(allocationStep);
+
+  memoryLeakAllocations.push(allocation);
+
+  const mu = process.memoryUsage();
+  // # bytes / KB / MB / GB
+  const gbNow = mu[field] / 1024 / 1024 / 1024;
+  const gbRounded = Math.round(gbNow * 100) / 100;
+
+  console.log(`Heap allocated ${gbRounded} GB`);
+}, TIME_INTERVAL_IN_MSEC);
